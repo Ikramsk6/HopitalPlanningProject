7f07c00d06c6014ed7cedcbd25c844c78931b9e3
ChartonMatthieu <153715941+ChartonMatthieu@users.noreply.github.com>
2025-02-12 22:02:42 +0100

    feat (mapper)
    add manual mapping for Besoin entity

--- src/main/java/com/example/HopitalPlanningProject/mappers/BesoinMapper.java
+++ src/main/java/com/example/HopitalPlanningProject/mappers/BesoinMapper.java
@@ -1,4 +1,27 @@
 package com.example.HopitalPlanningProject.mappers;
 
+import com.example.HopitalPlanningProject.DTO.BesoinDTO;
+import com.example.HopitalPlanningProject.models.Besoin;
+
 public class BesoinMapper {
+
+    public static BesoinDTO toDTO(Besoin besoin) {
+        if (besoin == null) {
+            return null;
+        }
+        BesoinDTO dto = new BesoinDTO();
+        dto.setId(besoin.getId());
+        dto.setBesoinsParShift(besoin.getBesoinsParShift());
+        return dto;
+    }
+
+    public static Besoin toEntity(BesoinDTO besoinDTO) {
+        if (besoinDTO == null) {
+            return null;
+        }
+        Besoin besoin = new Besoin();
+        besoin.setId(besoinDTO.getId());
+        besoin.setBesoinsParShift(besoinDTO.getBesoinsParShift());
+        return besoin;
+    }
 }
