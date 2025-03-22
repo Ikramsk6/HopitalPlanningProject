c351d2be0e965848929c822d3cbbadc1e0d84ac7
Ikramsk6 <ikram_souki@outlook.fr>
2025-03-22 18:01:52 +0100

    ajout des repositories

---
+++ src/main/java/com/example/HopitalPlanningProject/controllers/PreferencePersonnelController.java
@@ -0,0 +1,26 @@
+package com.example.HopitalPlanningProject.controllers;
+
+import com.example.HopitalPlanningProject.model.PreferencePersonnel;
+import com.example.HopitalPlanningProject.services.PreferencePersonnelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/preference-personnel")
+public class PreferencePersonnelController {
+
+    @Autowired
+    private PreferencePersonnelService service;
+
+    @GetMapping
+    public List<PreferencePersonnel> getAll() {
+        return service.getAll();
+    }
+
+    @PostMapping
+    public PreferencePersonnel create(@RequestBody PreferencePersonnel preference) {
+        return service.create(preference);
+    }
+}
